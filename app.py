9608007657d83051ef69e745a580218a121e760e
arthxr4 <130701288+arthxr4@users.noreply.github.com>
2024-04-24 16:35:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,20 @@ import logging
 
 app = FastAPI()
 
-# Configurer le logging
+# Set up logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @app.get("/fetch-email-html")
-def fetch_email_html(imap_address: str, username: str, password: str, uid: str):
+def fetch_email_html(imap_address: str, username: str, password: str, email_uid: str):
     """Endpoint to fetch the HTML content of an email by UID."""
     try:
         with Imbox(imap_address, username=username, password=password, ssl=True) as imbox:
             # Fetch email by UID
-            messages = imbox.messages(uid=uid)
-            if not messages:
-                return {"status": "failure", "message": "No email found with the provided UID."}
+            messages = imbox.messages(uid=email_uid)
             for uid, message in messages:
-                # Check if 'html' key exists in the body, and if it has content
                 html_content = message.body.get('html', [])[0] if message.body.get('html') else "No HTML content available"
                 return {"status": "success", "uid": uid, "html_content": html_content}
+            return {"status": "failure", "message": "No email found with the provided UID."}
     except Exception as e:
         logging.error("An error occurred while trying to fetch the email:", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
