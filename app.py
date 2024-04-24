c8254e408cffb366031b16cf2df0d0d25330e8bb
arthxr4 <130701288+arthxr4@users.noreply.github.com>
2024-04-24 16:04:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from imbox import Imbox
+import logging
 
 app = FastAPI()
+
+# Configurer le logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @app.get("/fetch-email")
 def fetch_email_content(imap_address: str, username: str, password: str, uid: str):
@@ -14,6 +18,7 @@ def fetch_email_content(imap_address: str, username: str, password: str, uid: st
                 return {"uid": uid, "html_content": html_content}
         return {"message": "Email with specified UID not found"}
     except Exception as e:
+        logging.error("An error occurred while trying to fetch the email:", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
